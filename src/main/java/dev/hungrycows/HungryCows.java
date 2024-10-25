991a47aa83aebd52e8bd3ff68f6859abf60deeb5
stlouisn <stlouisn@hotmail.com>
2024-10-24 21:18:25 -0300

    Update HungryCows.java

--- src/main/java/dev/hungrycows/HungryCows.java
+++ src/main/java/dev/hungrycows/HungryCows.java
@@ -1,6 +1,6 @@
 package dev.hungrycows;
 
-import dev.hungrycows.utils.ModConstants;
+import dev.hungrycows.utils.Constants;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import org.slf4j.Logger;
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 public class HungryCows implements ModInitializer {
 
   @SuppressWarnings("unused")
-  public static final Logger LOGGER = LoggerFactory.getLogger(ModConstants.MOD_ID);
+  public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
 
   public static EntityDataAccessor<Byte> IS_MILKED;
 
